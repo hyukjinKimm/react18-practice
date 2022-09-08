a966ba778ae06a5c91c806d69263aaf3e623c19f
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-08 20:15:44 +0900

    setTimeout 넣어 반응속도체크

--- 4.반응속도체크/ResponseCheck-class.jsx
+++ 4.반응속도체크/ResponseCheck-class.jsx
@@ -3,11 +3,46 @@ import React, { PureComponent } from 'react'
 class ResponseCheck extends PureComponent {
   state = {
     state: 'waiting',
-    message: '클릭해서 시작하세요',
+    message: '클릭해서 시작하세요!',
     result: []
   }
+  timeout;
+  startTime;
+  endTime;
   onClickScreen = (e) => {
+    const { state, message, result } = this.state
+    if (state === 'waiting'){
+        this.setState({
+          state: 'ready',
+          message: '초록색이 되면 클릭하세요'
+        })
+        this.timeout = setTimeout(() => {
+          this.setState({
+            state: 'now',
+            message: '지금 클릭!'
+          })
+          this.startTime = new Date()
+        }, Math.floor(Math.random() * 1000 + 2000));  
 
+    } else if(state === 'ready'){  // 성급하게 클릭
+        clearTimeout(this.timeout)
+        this.setState({
+            state: 'waiting',
+            message: '너무 성급하시군요. 초록색이되면 클릭하세요. 다시 클릭해서 시작하세요!'
+        }) 
+
+    } else if (state == 'now'){ // 반응속도 체크
+        this.endTime = new Date()
+        this.setState((preveState) => {
+            return {
+                state: 'waiting',
+                message: '클릭해서 시작하세요!',
+                result: [...preveState.result, this.endTime - this.startTime]
+            }
+        })
+    
+
+    }
   }
  
   renderAverage = () => {
