7c72e64d9cf2ffff75c3724d437f10bb8ff476d1
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-09 21:38:01 +0900

    setInterval과 라이프사이클 연동하기

--- 5.가위바위보/RSP-class.jsx
+++ 5.가위바위보/RSP-class.jsx
@@ -1,20 +1,52 @@
 import React, { PureComponent } from "react";
+const rspCoords = {
+  바위: '0',
+  가위: '-142px',
+  보: '-284px'
+};
+
+const scores = {
+  가위 : 1,
+  바위: 0,
+  보: -1
+}
 class RSP extends PureComponent{
   state = {
     result: '',
-    imgCoord: 0,
+    imgCoord: '0',
     score: 0,
   }
   // 클래스의 경우 -> constructor -> render -> ref렌더링 처리 -> componentDidMount
-// (setState/props 바뀔때) -> shouldComponentUpdate(true) -> Rerender -> componentDidUpdate
-// 부모가 나를 없앴을 때 -> componentWillUnmount -> 소멸
-  componentDidMount(){ // 첫 렌더링 시
+  // (setState/props 바뀔때) -> shouldComponentUpdate(true) -> Rerender -> componentDidUpdate
+  // 부모가 나를 없앴을 때 -> componentWillUnmount -> 소멸
+  interval;
+  componentDidMount(){ // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이 해요
 
+    this.interval = setInterval(() => {
+      const { imgCoord } = this.state 
+      console.log(imgCoord)
+      if ( imgCoord === rspCoords.바위){
+        this.setState({
+          imgCoord: rspCoords.가위
+        })
+      } else if(imgCoord === rspCoords.가위){
+        this.setState({
+            imgCoord: rspCoords.보
+          })     
+      } else if(imgCoord === rspCoords.보){
+        this.setState({
+            imgCoord: rspCoords.바위
+          });             
+      }
+    }, 1000);
   }
   componentDidUpdate(){ // 이후의 리 렌더링시
 
   }
-  componentWillUnmount(){ // 부모에 의해서 내가 없어질때 
+  componentWillUnmount(){ // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 해요
+    clearInterval(this.interval)
+  }
+  onClickBtn = (choice) => {
 
   }
   render(){
