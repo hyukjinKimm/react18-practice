b650239c62646371ae76595a8c0a0e31cc8ac9cd
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-13 16:50:44 +0900

    틱택토 구현하기

--- 7.틱택토/Td-function.jsx
+++ 7.틱택토/Td-function.jsx
@@ -1,11 +1,13 @@
 import React, { memo, useCallback } from "react";
-import { CLICK_CELL, CHANGE_TURN } from "./TicTacToe-function";
+import { CLICK_CELL } from "./TicTacToe-function";
 const Td = memo(({rowIndex, cellIndex, dispatch, cellData }) => {
   const onclickTd = useCallback(() => {
     console.log(rowIndex, cellIndex)
+    if(cellData){
+      return;
+    }
     dispatch({type: CLICK_CELL, row: rowIndex, cell: cellIndex})
-    dispatch({type: CHANGE_TURN})
-  }, [])
+  }, [cellData])
   return(
     <React.Fragment>
       <td onClick={onclickTd}>{cellData}</td>
