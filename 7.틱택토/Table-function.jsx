db4ac55720e3929d40c98479166f3f69066067b9
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-13 18:23:16 +0900

    테이블 최적화하기

--- 7.틱택토/Table-function.jsx
+++ 7.틱택토/Table-function.jsx
@@ -2,12 +2,12 @@ import React, { memo } from "react";
 import Tr from './Tr-function'
 
 const Table = memo(({ onClick, tableData, dispatch } ) => {
-
+  console.log('table renderd')
   return(
     <React.Fragment>
       <table onClick={onClick}>
         <tbody>
-          {Array(tableData.length).fill().map((v, i) => (<Tr dispatch={dispatch} rowIndex ={i} rowData={tableData[i]}/>))}
+          {Array(tableData.length).fill().map((v, i) => (<Tr key={i} dispatch={dispatch} rowIndex ={i} rowData={tableData[i]}/>))}
         </tbody>
       </table>
   </React.Fragment>
--- 7.틱택토/Tr-function.jsx
+++ 7.틱택토/Tr-function.jsx
@@ -2,11 +2,11 @@ import React, { memo } from "react";
 import Td from './Td-function'
 
 const Tr = memo(({ rowData, rowIndex, dispatch }) => {
-
+  console.log('tr rendered')
   return(
     <React.Fragment>
       <tr>
-        {Array(rowData.length).fill().map((v,i) => <Td dispatch={dispatch} rowIndex={rowIndex} cellIndex = {i} cellData={rowData[i]}/>)}
+        {Array(rowData.length).fill().map((v,i) => <Td key={i} dispatch={dispatch} rowIndex={rowIndex} cellIndex = {i} cellData={rowData[i]}/>)}
       </tr>
   </React.Fragment>
   )
