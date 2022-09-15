3a97af219a54ddb898fa7555a7b47629bc325350
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-15 17:45:49 +0900

    빈칸들 한번에 열기

--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -69,20 +69,60 @@ const reducer = (state, action) => {
     }
     case OPEN_CELL: {
       const tableData  = [...state.tableData]
-      tableData[action.row] = [...state.tableData[action.row]]
-      tableData[action.row][action.cell] = CODE.OPENED
-      let around = []
-      if (tableData[action.row - 1]){
-        around = around.concat(tableData[action.row - 1][action.cell -1], tableData[action.row - 1][action.cell], tableData[action.row - 1][action.cell +1])
+      tableData.forEach((row, i) => {
+        tableData[i] = [...state.tableData[i]]
+      })
+      const checked = [];
+      console.log(tableData.length, tableData[0].length);
+      const checkAround = (row, cell) => {
+        console.log(row, cell);
+        if (cell < 0 || cell >= tableData[0].length) {
+          return;
+        } // 상하좌우 없는칸은 안 열기
+        if ([CODE.OPENED, CODE.FLAG, CODE.QUESTION].includes(tableData[row][cell])) {
+          return;
+        } // 닫힌 칸만 열기
+        if (checked.includes(row + '/' + cell)) {
+          return;
+        } else {
+          checked.push(row + '/' + cell);
+        } // 한 번 연칸은 무시하기
+        let around = [
+          tableData[row][cell - 1], tableData[row][cell + 1],
+        ];
+        if (tableData[row - 1]) {
+          around = around.concat([tableData[row - 1][cell - 1], tableData[row - 1][cell], tableData[row - 1][cell + 1]]);
+        }
+        if (tableData[row + 1]) {
+          around = around.concat([tableData[row + 1][cell - 1], tableData[row + 1][cell], tableData[row + 1][cell + 1]]);
+        }
+        const count = around.filter(function (v) {
+          return [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v);
+        }).length;
+        if (count === 0) { // 주변칸 오픈
+            const near = [];
+            if (row - 1 > -1) {
+              near.push([row -1, cell - 1]);
+              near.push([row -1, cell]);
+              near.push([row -1, cell + 1]);
+            }
+            near.push([row, cell - 1]);
+            near.push([row, cell + 1]);
+            if (row + 1 < tableData.length) {
+              near.push([row + 1, cell - 1]);
+              near.push([row + 1, cell]);
+              near.push([row + 1, cell + 1]);
+            }
+            near.forEach((n) => {
+              if (tableData[n[0]][n[1]] !== CODE.OPENED) {
+                checkAround(n[0], n[1]);
+              }
+            })
+          
+        }
+        tableData[row][cell] = count;
       }
-      around = around.concat(tableData[action.row][action.cell -1], tableData[action.row][action.cell +1])
-      if (tableData[action.row + 1]){
-        around = around.concat(tableData[action.row + 1][action.cell -1], tableData[action.row + 1][action.cell], tableData[action.row + 1][action.cell +1])
-      }   
-      const count = around.filter((v) => [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)).length;
-      tableData[action.row][action.cell] = count  
-  
-
+      checkAround(action.row, action.cell)
       return {
         ...state,
         tableData
